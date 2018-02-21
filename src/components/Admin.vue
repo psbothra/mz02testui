cd05b6eac38868ecef191b77c74e19c891073ff1
amicablecoder <ankit@mindzcloud.com>
2018-02-21 23:26:03 +0530

    Updated db

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,71 +1,55 @@
 <template>
-  <div class="admin">
+  <div>
 
-    <div>
-      <v-layout row wrap>
-        <v-flex xs6>
-    <v-btn @click="dialog=true" flat fab>
-      <v-icon>file_upload</v-icon>
-    </v-btn>Upload Doc
-  </v-flex>
-</v-layout>
 
-    <v-dialog v-model="dialog" maxWidth="600">
-      <v-card class="pa-4" style="background-color: #0000">
-        <v-card flat v-show=" docUrl != '' ">
-          <a :href="docUrl" target="_blank">Click to view file
-          </a>
-        </v-card>
+    <v-dialog v-model="dialog" maxWidth="400">
+      <v-card class="pa-4" >
+        <v-text-field
+          label="Name of course"
+          v-model="course_name">
+        </v-text-field>
+
+        <v-text-field
+          label="Short Description"
+          v-model="course_description"
+          multi-line>
+        </v-text-field>
 
         <v-card-actions>
-
           <label id="#bb1" style="margin-left: 17px;">
             <v-icon class="white--text">
               file_upload
             </v-icon>
-            <input type="file" name="doc" accept="*" @change="uploaddoc($event)" id="uploaddoc">
+            <input type="file" name="doc" accept="*"  @change="uploaddoc($event)" id="uploaddoc">
           </label>
-
+          <span class="caption">Upload Document</span>
         </v-card-actions>
 
-        <btnLoader v-if="btnLoader"></btnLoader>
-        <v-btn flat v-else @click="uploaddocument(docObj, docUrl)">
-          Add
-        </v-btn>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+            <v-btn flat @click="uploaddata({name: course_name,
+              description: course_description,
+              docObj: docObj,
+              docUrl: docUrl})">
+              Add
+            </v-btn>
+        </v-card-actions>
 
       </v-card>
     </v-dialog>
-  </div>
 
-  <div>
-      <v-layout row wrap>
-        <v-flex xs6>
-      <v-btn @click="callupload()" flat fab>
-        <v-icon>file_upload</v-icon>
-      </v-btn>Upload Video
-    </v-flex>
-</v-layout>
-  </div>
-  <v-dialog v-model="d1" style="width:600; height:200">
-    <form method="POST"
-    :action=video_redirect
-    enctype="multipart/form-data">
-      <label for="file" style="margin-left: 17px;">
-      <input type="file" name="file_data" id="file">
-      <br>
-      </label>
-      <input type="submit" name="submit" value="Submit">
-    </form>
-  </v-dialog>
+    <v-btn @click="dialog=true" class="fixed-floating-button white--text" fab absolute fixed
+    style="background-color:#35495E">
+       <v-icon>add</v-icon>
+     </v-btn>
+
   </div>
 </template>
 
 <script>
-  import axios from 'axios'
   import {mapMutations, mapGetters} from 'vuex'
 
 export default {
-    name: 'admin',
     props: ['auth', 'authenticated'],
     data () {
       return {
@@ -73,53 +57,17 @@ export default {
         docObj: '',
         docUrl: '',
         dialog: false,
-        d1: false
+        course_name: '',
+        course_description: ''
       }
     },
     methods: {
       ...mapMutations([
+        'uploaddata'
       ]),
       uploaddoc (event) {
-        console.log(event.target.files[0].mozFullPath)
         this.docObj = event.target.files[0]
         this.docUrl = URL.createObjectURL(event.target.files[0])
-      },
-      callupload () {
-        axios.get(`https://mz02testui.herokuapp.com/uploadvideo`)
-          .then(response => {
-            console.log(response.data)
-            this.video_redirect = response.data
-            this.d1 = true
-            // var res = str.replace(/255370320/g, "210")
-          })
-          .catch(e => {
-            console.log(e)
-          })
-      },
-
-      calldb () {
-        axios.get(`https://mz02testui.herokuapp.com/connection`)
-          .then(response => {
-            console.log(response.data)
-            this.db_connect = response.data
-          })
-          .catch(e => {
-            console.log(e)
-          })
-      },
-      uploaddocument (x, y) {
-        axios.get(`https://mz02testui.herokuapp.com/uploaddoc`, {
-          params: {
-            docObj: x,
-            docUrl: y
-          }
-        })
-          .then(response => {
-            console.log(response.data)
-          })
-          .catch(e => {
-            console.log(e)
-          })
       }
     },
     components: {
@@ -135,8 +83,19 @@ export default {
     }
 }
 </script>
+
 <style scoped>
-a {
-  cursor: pointer;
-}
+  a {
+    cursor: pointer;
+  }
+  label{
+    padding: 10px;
+    background: grey;
+    display: table;
+    color: #fff;
+    border-radius: 100%;
+  }
+  input[type="file"] {
+    display: none;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -8,10 +8,15 @@ import router from './router'
 import Vuetify from 'vuetify'
 import '../node_modules/vuetify/dist/vuetify.min.css'
 import './assets/cssLib/googleFont.css'
+import '../node_modules/vuetify/src/stylus/main.styl'
 import './assets/css/style.css'
 
 // store
 import {store} from './store'
+
+// firebase
+import {config} from './firebase/config'
+const firebase = require('firebase')
 
 Vue.config.productionTip = false
 Vue.use(Vuetify)
@@ -22,5 +27,9 @@ new Vue({
   router,
   store,
   template: '<App/>',
-  components: { App }
+  components: { App },
+  created () {
+    this.$store.state.gen.firebaseApp = firebase.initializeApp(config)
+    this.$store.state.gen.storage = firebase.storage()
+  }
 })
