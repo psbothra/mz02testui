426c427d112b53e0f52021ac3c11f1837c5a2a29
amicablecoder <ankit@mindzcloud.com>
2018-02-21 13:08:22 +0530

    Adding files.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,8 +3,8 @@
     <div align="centre" class= "pb-5">
       <h1> This is for demo purpose
       </h1>
-      <h4>
-        The admin panel here cntain the features to upload the documents and the videos.
+      <h4 class="pl-5 pr-5">
+        The admin panel here cntain the features to upload the documents and the videos.<br>
         The login button authenticates the user with the API auth0. And there you have access to the documents and the videos.
         The training data is fetched from the database mongodb.
     </h4>
