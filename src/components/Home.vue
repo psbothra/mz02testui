7d8f05f61338c31118b07eeebd4e357ab13daffc
amicablecoder <ankit@mindzcloud.com>
2018-03-12 16:46:42 +0530

    Blog

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <loader v-if="loader"></loader>
-    <div class= "pb-5 pt-5" align="center">
+    <div v-else class= "pb-5 pt-5" align="center">
       <h1> This is for demo purpose<br><br>
       </h1>
       <h4>
@@ -34,6 +34,26 @@
       </v-expansion-panel>
     </v-layout>
 </div>
+<div id="disqus_thread"></div>
+<script>
+
+// Change blog3 to the blog ID dynamically to load different threads
+var disqusConfig = function () {
+  this.page.url = 'http://mz02test-herokuapp/blog1'
+  // Replace PAGE_URL with your page's canonical URL variable
+  this.page.identifier = 'blog1'
+  // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+
+(function () { // DON'T EDIT BELOW THIS LINE
+  var d = document
+  var s = d.createElement('script')
+  s.src = 'https://mz02test-herokuapp.disqus.com/embed.js'
+  s.setAttribute('data-timestamp', +new Date());
+  (d.head || d.body).appendChild(s)
+})()
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
   </div>
 </div>
 </template>
