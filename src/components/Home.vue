0ae5b12a74d3c02d1213b0d1769015a824a3db21
amicablecoder <ankit@mindzcloud.com>
2018-03-13 13:57:58 +0530

    Blog

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -34,26 +34,6 @@
       </v-expansion-panel>
     </v-layout>
 </div>
-<div id="disqus_thread"></div>
-<script>
-
-// Change blog3 to the blog ID dynamically to load different threads
-var disqusConfig = function () {
-  this.page.url = 'http://mz02test-herokuapp/blog1'
-  // Replace PAGE_URL with your page's canonical URL variable
-  this.page.identifier = 'blog1'
-  // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-};
-
-(function () { // DON'T EDIT BELOW THIS LINE
-  var d = document
-  var s = d.createElement('script')
-  s.src = 'https://mz02test-herokuapp.disqus.com/embed.js'
-  s.setAttribute('data-timestamp', +new Date());
-  (d.head || d.body).appendChild(s)
-})()
-</script>
-<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
   </div>
 </div>
 </template>
